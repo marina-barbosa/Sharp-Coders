ef5db3940140a6acf0783ca4783eb0998e2bdffe
marina-barbosa <marinabarbosa.exp@gmail.com>
2023-12-17 02:23:54 -0300

    att tour dos herois

--- 04-Angular/tour-dos-herois/src/app/app.component.ts
+++ 04-Angular/tour-dos-herois/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeroesComponent } from "./heroes/heroes.component";
+import { MessagesComponent } from "./messages/messages.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, HeroesComponent]
+    imports: [CommonModule, RouterOutlet, HeroesComponent, MessagesComponent]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
