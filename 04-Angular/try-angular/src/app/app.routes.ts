4479c458d0769eaf484b17f061e96e4387793136
marina-barbosa <marinabarbosa.exp@gmail.com>
2023-12-18 17:01:59 -0300

    add funcao enviarFormulario

--- 04-Angular/try-angular/src/app/app.routes.ts
+++ 04-Angular/try-angular/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     {path:'', redirectTo:'/home', pathMatch:'full'},
-    {path:'**', component:HomeComponent},
     {path:'home', component:HomeComponent},
     {path:'contact', component:ContactComponent},
+    {path:'**', component:HomeComponent},
 ];
--- 04-Angular/try-angular/src/app/pages/contact/contact.component.ts
+++ 04-Angular/try-angular/src/app/pages/contact/contact.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatFormFieldModule, MatButtonModule, MatInputModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+
+  nome:string;
+  email:string;
+  mensagem:string;
+
+  constructor(){
+    this.nome='';
+    this.email='';
+    this.mensagem='';
+  }
+
+enviarFomulario() {
+  console.log(this.nome);
+  console.log(this.email);
+  console.log(this.mensagem);
+  
+}
 
 }
